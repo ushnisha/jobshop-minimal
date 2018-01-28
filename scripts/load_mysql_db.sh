ec534beb93b640778a34e9d5103b5c7a09410de4
Arun Kunchithapatham <arunk@debian-e6530.localdomain>
2018-01-28 11:14:56 -0600

    Fixes for Github Issue #35
    * Added updated copyright message using the template provided by GNU
    
    * Updated the copyright year to 2017-2018

--- scripts/load_mysql_db.sh
+++ scripts/load_mysql_db.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 ###############################################################################
-# Copyright (c) 2017 Arun Kunchithapatham                                     #
-# All rights reserved.  This program and the accompanying materials           #
-# are made available under the terms of the GNU AGPL v3.0                     #
-# which accompanies this distribution, and is available at                    #
-# https://www.gnu.org/licenses/agpl-3.0.en.html                               #
+# Copyright (c) 2017-2018 Arun Kunchithapatham                                #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published    #
+# by the Free Software Foundation, either version 3 of the License, or        #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU Affero General Public License for more details.                         #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
 # Contributors:                                                               #
 # Arun Kunchithapatham - Initial Contribution                                 #
 ###############################################################################
